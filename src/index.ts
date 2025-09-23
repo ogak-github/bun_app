6eb0c86c1c6d808efce012eb683711b74a320bd7
Yoga Novianto <yoga@intimap.com>
2025-09-23 10:10:23 +0700

    Run elysia in multiple thread

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/*
+  - Elysia is single threaded by default,
+  - below is a cluster config. This will make sure that Elysia is running on multiple CPU cores.
+*/
+
+import cluster from "node:cluster";
+import os from "node:os";
+import process from "node:process";
+
+if (cluster.isPrimary) {
+  // --- BAGIAN INI KITA UBAH ---
+  console.log(`Primary process ${process.pid} is running`);
+  console.log(`Forking server for ${os.availableParallelism()} CPUs...`);
+  console.log("🚀 Server ready at: http://localhost:3000"); //
+  for (let i = 0; i < os.availableParallelism(); i++) cluster.fork();
+} else {
+  await import("./server");
+  console.log(`Worker ${process.pid} started`);
+}
